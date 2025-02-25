d3e916d384629b67702a3bc1aaabf97a486f8509
Hasan Ashrafi <yasashrafi@gmail.com>
2025-02-25 17:54:42 +0330

    `Refactor Skills component: updated container class, list alignment, and hover effect`

--- components/Skills.js
+++ components/Skills.js
@@ -22,12 +22,12 @@ function Skills() {
   ];
 
   return (
-    <div id='skills' className="container mx-auto px-6 py-12 max-w-7xl">
+    <div id='skills' className=" container mx-auto px-4 md: py-12 max-w-7xl">
       <div>
         <p className="text-3xl font-bold mb-8">Skills</p>
-        <ul className="list-none flex items-center flex-wrap gap-x-3 mt-2 justify-center">
+        <ul className="list-none flex items-center flex-wrap gap-x-3 mt-2 justify-start">
           {frontendSkills.map((skill, index) => (
-            <li key={index} className="w-36 text-center bg-blue-500 rounded-full p-2 m-1 text-lg text-white">
+            <li key={index} className="hover:bg-blue-800 ease-in-out transition-all duration-500 dark:hover:bg-gray-800 dark:text-gray-300 dark:bg-gray-600 w-36 text-center bg-blue-500 rounded-full p-2 m-1 text-lg text-white">
               {skill}
             </li>
           ))}
