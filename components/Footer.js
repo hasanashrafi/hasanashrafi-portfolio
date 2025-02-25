712b9712eb1b718b734e961858b667c0cf81f2f9
Hasan Ashrafi <yasashrafi@gmail.com>
2025-02-25 17:54:10 +0330

    edit styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,10 @@
 // components/Footer.js
-import Link from 'next/link';
 
 const Footer = () => {
-  const githubUrl = 'https://github.com/yourusername';
-  const linkedinUrl = 'https://linkedin.com/in/yourusername';
-  const email = 'mailto:your.email@example.com';
-
+ 
   return (
     <footer className="bg-gray-800 text-white py-6 flex justify-center">
-      <div className="flex gap-4">
-        <Link href={githubUrl}>
-            GitHub
-         
-        </Link>
-        <Link href={linkedinUrl}>
-            LinkedIn
-    
-        </Link>
-        <Link href={email}>
-            Email
-         
-        </Link>
-      </div>
+     <p>Developed By HsN.DeV</p>
     </footer>
   );
 };
