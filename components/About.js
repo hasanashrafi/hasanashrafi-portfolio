00eb5045f91ac8f96780b537962830057a754425
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-02 21:52:44 +0330

    edit about me text

--- components/About.js
+++ components/About.js
@@ -8,12 +8,12 @@ function About() {
       <div className=' mx-auto flex flex-col md:flex-row items-center gap-x-3 '>
         <Image src="/profile.jpg" width={300} height={100} className='mb-5 rounded-full shadow-lg' alt="profile" priority />
         <div>
-          <p className='dark:text-gray-300 text-lg text-wrap text-opacity-80 text-gray-700'>I am a software developer with a passion for building scalable and
-            efficient systems. <br/>
-            I have experience with a variety of programming languages and
-            technologies, including JavaScript, Python, and React.<br/> I am always looking to learn
-            and improve my skills, and I am excited to contribute to a team that shares my
-            passion for innovation and excellence.</p>
+          <p className='dark:text-gray-300 text-lg text-wrap text-opacity-80 text-gray-700'>
+            I am a software developer with a passion for building scalable and efficient systems.
+            I have experience with a  programming languages and
+            technologies, including JavaScript, and React. I am always looking to learn
+            and improve my skills
+          </p>
         </div>
       </div>
     </div>
