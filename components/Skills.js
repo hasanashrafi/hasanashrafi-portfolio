fc3473940b87ab6c4aacf7cdb7c9150c0751b537
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-02 21:54:56 +0330

    show skills with skills card and props data

--- components/Skills.js
+++ components/Skills.js
@@ -1,38 +1,22 @@
+
 // components/Skills.js
+import { frontendSkills } from '@/data/data';
+
 import React from 'react';
+import SkillCard from './templates/SkillCard';
 
 function Skills() {
-  const frontendSkills = [
-    'SPA',
-    'MUI',
-    'API',
-    'RWD',
-    'React',
-    'Redux',
-    'Next.js',
-    'mongoDB',
-    'HTML/CSS',
-    'bootstrap',
-    'JavaScript',
-    'Git-Github',
-    'ReactQuery',
-    'TailwindCss',
-    'Accessibility',
-    'Redux-toolkit',
-  ];
+ 
 
   return (
-    <div id='skills' className=" container mx-auto px-4 md: py-12 max-w-7xl">
-      <div>
+    <div id='skills' className="container mx-auto px-2 md:py-12 max-w-7xl">
         <p className="text-3xl font-bold mb-8">Skills</p>
-        <ul className="list-none flex items-center flex-wrap gap-x-3 mt-2 justify-center md:justify-start ">
+        <ul className="list-none flex items-center flex-wrap gap-x-2 mt-2 justify-center ">
           {frontendSkills.map((skill, index) => (
-            <li key={index} className="hover:bg-blue-800 ease-in-out transition-all duration-500 dark:hover:bg-gray-800 dark:text-gray-300 dark:bg-gray-600 w-36 text-center bg-blue-500 rounded-full p-2 m-1 text-lg text-white">
-              {skill}
-            </li>
+           <SkillCard key={index} skill={skill} index={index} />
           ))}
         </ul>
-      </div>
+    
     </div>
   );
 }
