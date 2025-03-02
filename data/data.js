3587bab206ab9d21d93ed86343d70dbb430b9a14
Hasan Ashrafi <yasashrafi@gmail.com>
2025-03-02 21:56:35 +0330

    export frontend skills data

---
+++ data/data.js
@@ -0,0 +1,19 @@
+ const frontendSkills = [
+    { name: 'HTML', image: "/images/html.png" },
+    { name: 'Css', image: "/images/css.png" },
+    { name: 'JavaScript', image: "/images/javascript.png" },
+    { name: 'React', image: "/images/react.png" },
+    { name: 'ReactQuery', image: "/images/reactquery.png" },
+    { name: 'Redux-toolkit', image: "/images/redux.png" },
+    { name: 'Next.js', image: "/images/nextjs.png" },
+    { name: 'TailwindCss', image: "/images/tailwind.png" },
+    { name: 'API', image: "/images/api.png" },
+    { name: 'RWD', image: "/images/responsive-design.png" },
+    { name: 'mongoDB', image: "/images/mongodb.png" },
+    { name: 'bootstrap', image: "/images/bootstrap.png" },
+    { name: 'Github', image: "/images/github.png" },
+    { name: 'Git', image: "/images/git.png" },
+  ];
+
+
+  export {frontendSkills}
