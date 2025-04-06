8270d8306d9536cbc746335432f9f4505693044c
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-06 21:55:50 +0330

    `feat: updated Projects component with framer-motion animations`

--- components/Projects.js
+++ components/Projects.js
@@ -1,15 +1,34 @@
-
-import ProjectCard from './templates/ProjectCard'
+'use client'
+import { motion } from 'framer-motion';
+import ProjectCard from './templates/ProjectCard';
 
 export default function Projects({ projects }) {
   return (
-    <section id="projects" className="container mx-auto px-6 py-12">
-      <p className="text-3xl font-bold mb-8">Featured Projects</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((project,) => (
-        <ProjectCard project={project}  key={project.id}/> 
-        ))}
-      </div>
-    </section>
-  )
+    <motion.div
+      initial={{ x: 500, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+        duration: 0.5,
+        delay: 0.1,
+        ease: 'easeInOut',
+      }}
+      className="mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 items-center"
+    >
+      {projects.map((project, index) => (
+        <motion.div
+          key={index}
+          initial={{ x: 500, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            duration: 0.5,
+            delay: index * 0.1,
+            ease: 'easeInOut',
+          }}
+          className=" dark:bg-gray-800  rounded-lg p-2"
+        >
+       <ProjectCard project={project} />
+        </motion.div>
+      ))}
+    </motion.div>
+  );
 }
