a3372f9af1c439952891a7f2bf40c72064bfa694
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-06 19:31:01 +0330

    `Refactor SkillCard component to use motion/react-client and add hover animation`

--- components/templates/SkillCard.js
+++ components/templates/SkillCard.js
@@ -1,19 +1,35 @@
-import React from 'react'
+'use client'
+import React from 'react';
 import Image from 'next/image';
+import * as motion from "motion/react-client"
 
-function SkillCard({skill}) {
+
+function SkillCard({ skill }) {
   return (
-    <div>
-     <ul >
-              <li className="flex flex-col items-center ease-in-out hover:scale-110 transition-all duration-500  dark:text-gray-300  w-36 text-center  text-black rounded-full p-2 m-2 text-sm ">
-              <Image width={50} height={50} src={skill.image} loading='lazy' alt={skill.name} />
-               <span >
-                {skill.name}
-               </span>
-              </li>
-            </ul>
-    </div>
-  )
+    <motion.div
+      whileHover={{
+                scale: [null, 1.1, 1.2],
+                transition: {
+                    duration: 0.5,
+                    times: [0, 0.6, 1],
+                    ease: ["easeInOut", "easeOut"],
+                },
+            }}
+            transition={{
+                duration: 0.3,
+                ease: "easeOut",
+            }}
+    >
+      <div className=' flex flex-col items-center justify-center '>
+        <li className=" flex justify-center   dark:text-gray-300  w-36 text-center  text-black rounded-full p-2 text-sm ">
+          <Image width={50} height={50} src={skill.image} loading='lazy' alt={skill.name} />
+        </li>
+        <p className='text-center mx-auto'>
+          {skill.name}
+        </p>
+      </div>
+    </motion.div>
+  );
 }
 
-export default SkillCard
+export default SkillCard;
