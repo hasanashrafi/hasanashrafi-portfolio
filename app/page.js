650864387ae64cd34f2ac24d13ca0c6e4d1d19d1
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-06 22:04:57 +0330

    `Update Home component to log projects and remove unnecessary whitespace`

--- app/page.js
+++ app/page.js
@@ -7,17 +7,17 @@ import { getRepositories } from '@/lib/github'
 
 export default async function Home() {
   const projects = await getRepositories()
-
+  console.log(projects)
   return (
     <div className="min-h-screen dark:bg-gray-900   dark:text-gray-300 space-y-10 ">
-     <div className='backdrop-blur-3xl'>
+      <div className='backdrop-blur-3xl'>
 
-      <Hero />
-      <About />
-      <Skills />
-      <Projects projects={projects} />
-      <Contact />
-     </div> 
+        <Hero />
+        <About />
+        <Skills />
+        <Projects projects={projects} />
+        <Contact />
+      </div>
     </div>
   )
 }
