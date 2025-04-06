5aae6bb810237ca45740ab75bc597cd83304c092
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-06 19:30:50 +0330

    edit styles

--- components/templates/ProjectCard.js
+++ components/templates/ProjectCard.js
@@ -5,17 +5,17 @@ import React from 'react'
 function ProjectCard({ project }) {
   const { name, description, stargazers_count, forks_count, language, html_url, homepage } = project
   return (
-    <div className=" dark:bg-gray-900 dark:border-none shadow-md border rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
+    <div className="backdrop-blur-3xl bg-white/15 text-transparent dark:bg-gray-800 dark:border-none shadow-md  rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
       <div className="p-6">
-        <p className="text-xl font-semibold mb-2">
+        <p className="text-white text-xl font-semibold mb-2">
           {name}
         </p>
-        <p className="text-gray-600 mb-4 line-clamp-2">
+        <p className="text-gray-50 mb-4 line-clamp-2">
           {description}
         </p>
 
         {/* Project Stats */}
-        <div className="flex items-center gap-4 text-sm text-gray-500 mb-4">
+        <div className="flex items-center gap-4 text-sm text-gray-50 mb-4">
           <span className="flex items-center gap-1 dark:text-gray-300">
             ⭐ {stargazers_count}
           </span>
@@ -34,7 +34,7 @@ function ProjectCard({ project }) {
           <Link
             href={html_url}
             rel="noopener noreferrer"
-            className=" dark:text-gray-300 flex items-center gap-2 text-gray-600 dark:hover:text-blue-500 ease-in-out transition-all hover:text-blue-500"
+            className=" dark:text-gray-300 flex items-center gap-2 text-gray-50 dark:hover:text-blue-500 ease-in-out transition-all hover:text-blue-500"
           >
             <Github className="h-4 w-4" />
             Code
@@ -43,7 +43,7 @@ function ProjectCard({ project }) {
             <Link
               href={homepage}
               rel="noopener noreferrer"
-              className=" dark:text-gray-300 flex items-center gap-2  text-gray-600 dark:hover:text-blue-500 ease-in-out transition-all hover:text-blue-500"
+              className=" dark:text-gray-300 flex items-center gap-2  text-gray-50 dark:hover:text-blue-500 ease-in-out transition-all hover:text-blue-500"
             >
               <ExternalLink className="h-4 w-4" />
               Demo
