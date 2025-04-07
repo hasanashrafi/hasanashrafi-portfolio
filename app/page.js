54cdfde2844b38fb4e8ed4c11cf83a0ac99c672d
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-07 13:32:26 +0330

    `feat: updated page.js and hero.js with minor changes`

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { getRepositories } from '@/lib/github'
 
 export default async function Home() {
   const projects = await getRepositories()
-  console.log(projects)
+ 
   return (
     <div className="min-h-screen dark:bg-gray-900   dark:text-gray-300 space-y-10 ">
       <div className='backdrop-blur-3xl'>
--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,5 @@
 import { Github, Linkedin, Mail } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 function Hero() {
@@ -6,18 +7,19 @@ function Hero() {
 
   return (
     <div className=" container mx-auto px-6 pt-32 pb-12">
-      <div className="max-w-3xl">
-        <div>
+      <div className="max-w-7xl">
+        <div className='w-full  flex items-center justify-between'>
           <p className="mb-10 dark:animate-none animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white dark:border-r-gray-800 pr-5 text-4xl  dark:text-gray-300 font-bold">
-            Hi, I am <br/>
+            Hi, I am <br />
             <span className=" bg-clip-text bg-gradient-to-r from-indigo-400  to-pink-400 text-transparent">Hasan Aashrafi</span>
           </p>
+          <Image src="/images/banner.webp" alt='banner' width={350} height={350} priority className='' />
 
-          <p className="  dark:text-gray-100 text-xl text-gray-50 mb-8">
-            A passionate Frontend Developer crafting beautiful and functional web experiences.
-            I specialize in React, Next.js, and modern web technologies.
-          </p>
         </div>
+        <p className="  dark:text-gray-100 text-xl text-gray-50 mb-8">
+          A passionate Frontend Developer crafting beautiful and functional web experiences.
+          I specialize in React, Next.js, and modern web technologies.
+        </p>
         <div className="flex items-center gap-4">
           <a
             href="#contact"
