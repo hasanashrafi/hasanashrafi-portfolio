5197babfa49adea4a4aa1629deb49bd0aa4175af
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-07 13:38:51 +0330

    `Refactor Hero component to add animation duration and Navbar component to remove unused import and simplify state management`

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,21 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { Moon, Sun } from 'lucide-react'
 import Image from 'next/image'
 
-export default function Navbar() {
-  const [darkMode, setDarkMode] = useState(() => {
-    const storedDarkMode = localStorage.getItem('darkMode')
-    return storedDarkMode === 'true' ? true : false
-  })
 
-  useEffect(() => {
-    document.body.classList.toggle('dark', darkMode)
-  }, [darkMode])
+export default function Navbar() {
+  const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode)
-    localStorage.setItem('darkMode', !darkMode)
-  }
-
+    setDarkMode(!darkMode);
+    document.body.classList.toggle('dark');
+  };
   return (
-    <header className="p-3 fixed top-0 left-0 right-0 z-50 text-white dark:bg-inherit dark:border-b-2 border-b-gray-800 backdrop-blur-md">
-      <nav className="w-full mx-auto px-5">
+    <header className=" p-3 fixed top-0 left-0 right-0  z-50 text-white dark:bg-inherit dark:border-b-2 border-b-gray-800  backdrop-blur-md ">
+      <nav className="w-full mx-auto  px-5">
         <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
           <Link
             href="https://hasanashrafi-portfolio.vercel.app/"
@@ -30,12 +23,13 @@ export default function Navbar() {
           >
             <Image
               src="/logo.webp"
-              alt="logo"
+              alt='logo'
               width={100}
               height={100}
               priority
-              className="size-10 text-white"
+              className='size-10 text-white'
             />
+
           </Link>
 
           <button
@@ -48,6 +42,11 @@ export default function Navbar() {
               <Moon className="text-gray-50" size={24} />
             )}
           </button>
+
+
+
+
+
         </div>
       </nav>
     </header>
