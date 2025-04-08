98727dddd97601bb4b90b6863b30432d05b99d49
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-08 12:45:08 +0330

    `Removed unused import from Navbar.js`

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useMemo, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Link from 'next/link';
 import { Moon, Sun } from 'lucide-react';
 import Image from 'next/image';
