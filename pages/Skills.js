f63f1cce631bb948ebac3709e678a9ba25875161
Hasan Ashrafi <yasashrafi@gmail.com>
2025-04-17 09:56:45 +0330

    edit styles in dark mode

--- pages/Skills.js
+++ pages/Skills.js
@@ -9,8 +9,8 @@ function Skills() {
 
 
   return (
-    <div id='skills' className="container mx-auto p-2 md:py-12 max-w-7xl ">
-      <p className="text-3xl font-bold mb-8">Skills</p>
+    <div id='skills' className="container mx-auto px-6 md:py-12 max-w-7xl ">
+      <p className="text-3xl font-bold  mb-8 dark:text-gray-500">Skills</p>
       <ul className="list-none flex items-center gap-2 flex-wrap  md:justify-start justify-center ">
         {frontendSkills.map((skill, index) => (
           <SkillCard key={index} skill={skill} />
